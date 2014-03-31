c74ff2d035ed6246a38d5c7005cc2546737f0ab9
Henry Singleton <henry@henrysingleton.com>
2014-03-31 20:08:36 +1100

    adding custom page titles and associated tests

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,15 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+  	assert_select 'title', "Home"
   end
 
-  test "should get candle" do
+  test "should get candle page" do
     get :candle
     assert_response :success
+  	assert_select 'title', "Candle"
   end
-
+  
 end
+
+
